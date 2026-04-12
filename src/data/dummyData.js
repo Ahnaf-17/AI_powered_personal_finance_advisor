b8eae449f19eb8460c224abb7d82f50dc1c186c4
Ahnaf-17 <ahnaf.ahmed.42173@gmail.com>
2026-04-12 11:14:31 +1000

    tailwind added for responsie design

---
+++ src/data/dummyData.js
@@ -0,0 +1,5 @@
+export const expenses = [
+  { title: "Food", amount: 200, category: "Food" },
+  { title: "Transport", amount: 100, category: "Transport" },
+  { title: "Shopping", amount: 300, category: "Shopping" },
+];
